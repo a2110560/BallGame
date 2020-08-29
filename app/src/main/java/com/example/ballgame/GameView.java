f6eba1625fc527279c48feeda4a2e021c7fd9ac7
Gary <36412918+a2110560@users.noreply.github.com>
2020-08-29 13:52:36 +0800

    init project

---
+++ app/src/main/java/com/example/ballgame/GameView.java
@@ -0,0 +1,13 @@
+package com.example.ballgame;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class GameView extends View {
+    public GameView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
